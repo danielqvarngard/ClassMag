5ee6257c802568b72d9ae30255ed9351eb93710e
Daniel Qvarngård <danielqvarngard@gmail.com>
2021-01-15 09:07:14 +0100

    Project structure update to make room for environments

--- environments/ParallelTempering/ptProcs.hpp
+++ environments/ParallelTempering/ptProcs.hpp
@@ -1,3 +1,10 @@
+#ifndef CLASSMAG_ENVIRONMENTS_PTPROCS_HPP
+#define CLASSMAG_ENVIRONMENTS_PTPROCS_HPP
+
+#include "MonteCarlo/include/VectorModelManager.hpp"
 namespace classmag::environments{
-    
+    int mpiPT_hub(unsigned int seed);
+    int mpiPT_mc();
 }
+
+#endif
