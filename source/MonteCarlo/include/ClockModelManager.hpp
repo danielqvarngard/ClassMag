0a803adde881817bbf1de5b98dfb3032a70044b4
Daniel Qvarngård <danielqvarngard@gmail.com>
2021-02-07 15:21:30 +0100

    initiated work on clock model

---
+++ source/MonteCarlo/include/ClockModelManager.hpp
@@ -0,0 +1,11 @@
+#ifndef CLASSMAG_MONTECARLO_CLOCKMODELMANAGER_HPP
+#define CLASSMAG_MONTECARLO_CLOCKMODELMANAGER_HPP
+
+namespace classmag::montecarlo {
+    class ClockModelManager{
+        public:
+        
+    };
+}
+
+#endif
