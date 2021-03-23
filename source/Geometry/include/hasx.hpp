80c41c8dd6e61cb3957962545f830991ec2653b0
Daniel Qvarngård <danielqvarngard@gmail.com>
2021-03-23 15:22:43 +0100

    Fixed correlation between bipartite disordered lattices

--- source/Geometry/include/hasx.hpp
+++ source/Geometry/include/hasx.hpp
@@ -12,9 +12,12 @@ namespace classmag::geometry{
         auto result = chas0_bipartite(size);
         auto disp1 = Euclidean<3>({0.0, 0.0, 0.0});
         auto disp2 = Euclidean<3>({0.5, 0.5, 0.5});
-
-        result.append_(disorderedCubic<3>(size, occupancy, disp1, seed));
-        result.append_(disorderedCubic<3>(size, occupancy, disp2, seed));
+        std::mt19937 mt(seed);
+        std::uniform_int_distribution distr;
+        const auto seed1 = distr(mt);
+        const auto seed2 = distr(mt);
+        result.append_(disorderedCubic<3>(size, occupancy, disp1, seed1));
+        result.append_(disorderedCubic<3>(size, occupancy, disp2, seed2));
         return result;
     }
     
