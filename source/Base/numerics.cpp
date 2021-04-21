7a499f8cc3bbc582e35649615623a365e1cdaf6a
Daniel Qvarngård <danielqvarngard@gmail.com>
2021-04-21 11:02:54 +0200

    Wrote Shanks transform

--- source/Base/numerics.cpp
+++ source/Base/numerics.cpp
@@ -4,4 +4,14 @@ namespace classmag::base{
     double pi(){
         return M_PI;
     }
+
+    std::vector<double> shanks(std::vector<double> &x){
+        std::vector<double> s(x.size() - 2);
+        for (auto ii = 0u; ii < s.size(); ++ii){
+            auto num = (x[ii + 2] - x[ii + 1]) * (x[ii + 2] - x[ii + 1]);
+            auto denom = x[ii + 2] - 2.0 * x[ii + 1] + x[ii];
+            s[ii] = x[ii + 2] - num/denom;
+        }
+        return s;
+    }
 }
