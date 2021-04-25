6a81c1bc6d59ede18782b817bf1fd7bf95d29a03
Daniel Qvarngård <danielqvarngard@gmail.com>
2021-04-25 11:39:13 +0200

    MatrixLookup class written

--- source/Base/include/couplingLookup.hpp
+++ source/Base/include/couplingLookup.hpp
@@ -4,6 +4,8 @@
 #include <functional>
 #include <iostream>
 
+
+#include "Geometry/include/matrix.hpp"
 namespace classmag::base{
     class CouplingLookup{
         private:
@@ -16,6 +18,26 @@ namespace classmag::base{
             CouplingLookup(): n_sites_(0){};
             CouplingLookup(const CouplingLookup &src);
             double coupling_(unsigned int site1, unsigned int site2) const;
+    };
+
+    template<unsigned int spinDim>
+    class MatrixLookup{
+        public:
+        MatrixLookup(const unsigned int n_sites):
+        n_sites_(n_sites),
+        couplingTable_(
+            std::vector<geometry::matrix<spinDim,spinDim>>(
+                n_sites, 0.0*geometry::eye<spinDim>())
+            )
+        {};
+
+        geometry::matrix<spinDim,spinDim> coupling_(unsigned int ii, unsigned int jj){
+            return couplingTable_[n_sites_*site1 + site2];
+        };
+
+        private:
+        const unsigned int n_sites_ = 0u;
+        std::vector<geometry::matrix<spinDim,spinDim>> couplingTable_;
     };
 }
 
