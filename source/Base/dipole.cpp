9cf764396757145c94107cc111d35e8848f9daba
Daniel Qvarngård <danielqvarngard@gmail.com>
2021-04-27 17:47:22 +0200

    Ensured positive normalization for volume in reciprocal

--- source/Base/dipole.cpp
+++ source/Base/dipole.cpp
@@ -5,7 +5,6 @@ namespace classmag::base{
         const geometry::Euclidean<3> &r, 
         const geometry::Euclidean<3> &k, 
         const EwaldProfile &ep){
-        auto x = geometry::norm(k);
         
         auto size = ep.lattice_.getSize_();
         auto bravais = ep.lattice_.getBravais_();
@@ -14,9 +13,12 @@ namespace classmag::base{
         for (unsigned int ii = 0u; ii < 3; ++ii){
             V *= size[ii];
         }
-        V *= bravais[0] * geometry::cross(bravais[1], bravais[2]);
+        V *= abs(bravais[0] * geometry::cross(bravais[1], bravais[2]));
 
-        return (4.0 * pi() / V) * (cos(k*r) / (x * x)) * exp(-x*x/(4*ep.alpha_)) * geometry::extprod(k,k);
+        auto x = geometry::norm(k);
+        return (4.0 * pi() / V) *
+            (cos(k*r) / (x * x)) * 
+            exp(-x*x/(4*ep.alpha_)) * geometry::extprod(k,k);
     }
 
     double ewaldRealB(const double r, const EwaldProfile &ep){
