10f7088db6647ccdcb3816a4e91e2285531fda7e
Daniel Qvarngård <danielqvarngard@gmail.com>
2021-10-20 09:42:30 +0200

    Added default executables for future linking with fileio

---
+++ executables/isingpt.cpp
@@ -0,0 +1,21 @@
+#include "Environments/ParallelTempering/ptProcs.hpp"
+
+using namespace classmag;
+
+int main(int argc, char* argv[]){
+    MPI_Init(NULL,NULL);
+
+    int world_size;
+	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+	
+	int world_rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    if (world_size == 1)
+        return -1;
+    if (world_rank == 0)
+        environments::mpiPT_hub({1.0, 2.0}, 0);
+    else{
+        
+    }
+}
