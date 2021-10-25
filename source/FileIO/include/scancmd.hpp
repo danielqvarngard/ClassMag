90085c9190402541ea095a3286f7ccee5f5e243e
Daniel Qvarngård <danielqvarngard@gmail.com>
2021-10-25 19:38:47 +0200

    Added header guards

--- source/FileIO/include/scancmd.hpp
+++ source/FileIO/include/scancmd.hpp
@@ -1,6 +1,10 @@
+#ifndef CLASSMAG_FILEIO_SCANCMD_HPP
+#define CLASSMAG_FILEIO_SCANCMD_HPP
 #include <string>
 #include <sstream>
 
 namespace classmag::fileio{
     std::string get_cmd_flag_str(const std::string& flag);
 }
+
+#endif
