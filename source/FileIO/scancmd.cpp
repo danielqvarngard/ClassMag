dbb6da27c8ad185e1076a2ca36fc27b697d430cb
Daniel Qvarngård <danielqvarngard@gmail.com>
2021-10-25 19:54:40 +0200

    Minimal example of the command line readin

--- source/FileIO/scancmd.cpp
+++ source/FileIO/scancmd.cpp
@@ -4,8 +4,15 @@ namespace classmag::fileio{
     std::string get_cmd_flag_str(
         const int argc,
         const char* argv[], 
-        const std::string& flag)
+        const char* flag)
     {
-        
+        std::string str;
+        for (auto ii = 0; ii < argc; ++ii){
+            if (!strcmp(argv[ii], flag)){
+                str = argv[ii + 1];
+            }
+        }
+
+        return str;
     }
 }
