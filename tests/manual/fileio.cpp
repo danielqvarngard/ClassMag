ab5c87e3a43a1699014f584ec19f70736a05657c
Daniel Qvarngård <danielqvarngard@gmail.com>
2021-10-26 09:41:37 +0200

    Removed passed check

--- tests/manual/fileio.cpp
+++ tests/manual/fileio.cpp
@@ -6,7 +6,6 @@ using namespace classmag;
 
 int main(int argc, char* argv[]){
     auto file_name = fileio::get_cmd_flag_str(argc, argv, "-input");
-    if (file_name.empty())
-        std::cout << "Yeehaw\n";
+    
     return 0;
 }
