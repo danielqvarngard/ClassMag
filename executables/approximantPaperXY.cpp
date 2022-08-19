80389da291478799966b4945a79e9a9dc48e6af3
Daniel Qvarngård <danielqvarngard@gmail.com>
2022-08-19 11:47:34 +0200

    Approximant paper executable now in testable state

--- executables/approximantPaperXY.cpp
+++ executables/approximantPaperXY.cpp
@@ -13,7 +13,9 @@
 #include "Geometry/include/predefLattices.hpp"
 #include "Environments/ParallelTempering/ptProcs.hpp"
 #include "Environments/ParallelTempering/print_pt_return.hpp"
+#include "MonteCarlo/include/XYModelManager.hpp"
 #include "MonteCarlo/include/PredefAnisotropyVectors.hpp"
+#include "Base/include/default_orderparameters.hpp"
 
 using namespace classmag;
 
@@ -32,9 +34,17 @@ int main(int argc, char* argv[]){
         infile = "./input/default.mcinput";
     }
     
+    std::cout << infile << "\n";
+
+    auto outdir = fileio::get_cmd_flag_str(argc,argv, "-output_directory");
+    auto final_letter = outdir.back();
+    if (final_letter != '/' && !outdir.empty()){
+        outdir.push_back('/');
+    }
+    
     auto outfile = fileio::get_cmd_flag_str(argc,argv, "-outfile_name");
     if (outfile.empty()){
-        outfile = "./out/" + fileio::datestamp() + ".mcout";
+        outfile = outdir + fileio::datestamp() + ".mcout";
     }
     
 
@@ -49,6 +59,10 @@ int main(int argc, char* argv[]){
     auto betas = montecarlo::invert_vector_elements(temperatures);
     montecarlo::VectorModel_Profile mco;
 
+    
+    auto op = base::Magnetization();
+    auto staggered_magnetization = base::BipartiteStaggeredMagnetization(lattice.partitions_());
+    op.push_back(staggered_magnetization);
     fileio::readMCO(mco, infile);
     mco.n_sites_ = lattice.n_sites_();
     if (world_rank == 0){
@@ -59,7 +73,7 @@ int main(int argc, char* argv[]){
         input.betas_ = betas;
         input.measurement_runs_ = mco.measurement_;
         input.thermalization_runs_ = mco.thermalization_;
-        auto results = environments::MPI_PT_Hub(input);
+        auto results = environments::MPI_PT_Hub(input, op.size());
         environments::print_pt_return(results, fp);
     }
     else {
@@ -68,8 +82,10 @@ int main(int argc, char* argv[]){
         fileio::readLinearInteractions<geometry::Matrix<3,3>,3,3>(couplings, infile, lattice);
         auto anivecs = montecarlo::compute_tsai_easyplanes();
         auto mc_proc = montecarlo::XYModelManager<3>(couplings,anivecs);
+        
         mc_proc.theta_distr_width *= 0.1;
-        environments::mpiPT_mc<3>(mc_proc,mco);
+        mc_proc.addOrderParameter_(op);
+        environments::MPI_PT_Edge<3>(mc_proc,mco);
     }
     MPI_Finalize();
 }
